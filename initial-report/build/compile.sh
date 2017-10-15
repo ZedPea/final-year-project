a702008aca2768082ce8591cf97d1d5b4e4325a8
ZedPea <zp+github@zacharypalmer.co.uk>
2017-10-15 13:56:23 +0100

    Use harvard referencing style

--- initial-report/build/compile.sh
+++ initial-report/build/compile.sh
@@ -9,6 +9,5 @@ done
 cp -r ../logo.svg ../initial-report.tex ../pages/ .
 
 pdflatex --shell-escape -interaction=batchmode initial-report.tex &&
-bibtex initial-report.aux &&
-pdflatex --shell-escape -interaction=batchmode initial-report.tex &&
+biber initial-report.bcf &&
 pdflatex --shell-escape -interaction=batchmode initial-report.tex
