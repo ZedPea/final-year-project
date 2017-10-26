3f9c658ad8f263d912dbdcb3a36f2599b2c09c54
ZedPea <zp+github@zacharypalmer.co.uk>
2017-10-26 17:58:46 +0100

    Add console gui

--- final-report/build/compile.sh
+++ final-report/build/compile.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-FILES=../pages/*.tex
+FILES=../sections/*.tex
 
 for f in $FILES
 do
     aspell --home-dir=. --personal=dictionary.txt -t -c $f
 done
 
-cp -r ../logo.svg ../final-report.tex ../pages/ .
+cp -r ../logo.svg ../final-report.tex ../sections/ .
 
 pdflatex --shell-escape -interaction=batchmode final-report.tex &&
 biber final-report.bcf &&
